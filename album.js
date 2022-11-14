f41b51265d4a5d4337fc50657bfdcd7b4d41b92b
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2022-11-14 17:31:26 +0000

    get data function

---
+++ album.js
@@ -0,0 +1,16 @@
+function getData(search) {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "47b90c0b15msh1f32277e69c01c9p123c88jsn0c5b7d2ef6ab",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
+
+getData("greenday");
